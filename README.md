23a1c826f533225af5b569b2f3981747738370f0
miya0001 <miya@wpist.me>
2015-12-15 21:21:11 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Generate Movefile
+
+It generates [Wordmove's Movefile](https://github.com/welaika/wordmove) for AMIMOTO.
+
+## What's AMIMOTO
+
+[AMIMOTO is a High Performance WordPress Cloud Hosting on Amazon Web Services.](http://amimoto-ami.com/)
+
+```
+ssh ec2-user@<ip-address> curl -L https://raw.githubusercontent.com/amimoto-ami/generate_movefile/master/run.sh | sudo bash
+```
---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+cd /var/www/vhosts/$(curl -L http://169.254.169.254/latest/meta-data/instance-id)
+
+php -r 'require "wp-config.php";
+echo "default:\n";
+echo "  vhost: " . home_url() . "\n";
+echo "  wordpress_path: " . getcwd() . "\n";
+echo "\n";
+echo "  database:\n";
+echo "    name: " . DB_NAME . "\n";
+echo "    user: " . DB_USER . "\n";
+echo "    password: " . DB_PASSWORD . "\n";
+echo "    host: " . DB_HOST . "\n";
+echo "\n";
+'
